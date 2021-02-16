8fc6ac681bc11e776f99747c3c27552a9bf2cfc5
Marcelo Hashimoto <marcelo.hashimoto@gmail.com>
2021-02-16 09:21:41 -0300

    Wrap edit script in main function

--- edit.py
+++ edit.py
@@ -13,41 +13,46 @@ CONCAT_NAME = 'concat.txt'
 OUTPUT_NAME = 'concat.mp4'
 
 
-length = len(sys.argv)
+def main():
+    length = len(sys.argv)
 
-if length != 4:
-    print('Usage: python <dirname> <leftcrop> <rightcrop>'.format(sys.argv[0]))
-else:
-    dirname = sys.argv[1]
-    leftcrop = int(sys.argv[2])
-    rightcrop = int(sys.argv[3])
+    if length != 4:
+        print('Usage: python <dirname> <leftcrop> <rightcrop>'.format(sys.argv[0]))
+    else:
+        dirname = sys.argv[1]
+        leftcrop = int(sys.argv[2])
+        rightcrop = int(sys.argv[3])
 
-    os.chdir(dirname)
+        os.chdir(dirname)
 
-    filenames = []
-    for filename in sorted(os.listdir()):
-        if filename not in [INPUT_NAME, SOURCE_NAME, CONCAT_NAME, OUTPUT_NAME]:
-            if ' ' in filename:
-                src = filename
-                filename = filename.replace(' ', '_')
-                os.rename(src, filename)
-            filenames.append(filename)
+        filenames = []
+        for filename in sorted(os.listdir()):
+            if filename not in [INPUT_NAME, SOURCE_NAME, CONCAT_NAME, OUTPUT_NAME]:
+                if ' ' in filename:
+                    src = filename
+                    filename = filename.replace(' ', '_')
+                    os.rename(src, filename)
+                filenames.append(filename)
 
-    with open(INPUT_NAME) as file:
-        data = json.load(file)
+        with open(INPUT_NAME) as file:
+            data = json.load(file)
 
-    with open(SOURCE_NAME, 'w') as file:
-        file.write('//////////////////////////////////////////\n')
-        for time in data['times']:
-            file.write('{}\n'.format(time))
-        file.write('//////////////////////////////////////////\n')
+        with open(SOURCE_NAME, 'w') as file:
+            file.write('//////////////////////////////////////////\n')
+            for time in data['times']:
+                file.write('{}\n'.format(time))
+            file.write('//////////////////////////////////////////\n')
 
-    with open(CONCAT_NAME, 'w') as file:
-        start = 0
-        for filename, subtimes in zip(filenames, data['timeline']):
-            if subtimes:
-                file.write('file {}\n'.format(filename))
-                file.write('outpoint {}\n'.format(subtimes[-1] - start))
-                start = subtimes[-1]
+        with open(CONCAT_NAME, 'w') as file:
+            start = 0
+            for filename, subtimes in zip(filenames, data['timeline']):
+                if subtimes:
+                    file.write('file {}\n'.format(filename))
+                    file.write('outpoint {}\n'.format(subtimes[-1] - start))
+                    start = subtimes[-1]
 
-    subprocess.run(['ffmpeg', '-f', 'concat', '-i', CONCAT_NAME, '-filter:v', 'crop=iw-{}:ih:{}:0,scale=384:trunc((384/iw)*ih/2)*2'.format(leftcrop + rightcrop, leftcrop), OUTPUT_NAME])
+        subprocess.run(['ffmpeg', '-f', 'concat', '-i', CONCAT_NAME, '-filter:v', 'crop=iw-{}:ih:{}:0,scale=384:trunc((384/iw)*ih/2)*2'.format(leftcrop + rightcrop, leftcrop), OUTPUT_NAME])
+
+
+if __name__ == '__main__':
+    main()
